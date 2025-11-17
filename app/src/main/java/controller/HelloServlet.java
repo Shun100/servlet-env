691510140d6ebb59084bee90c1500f9f27865e09
Shun Takamura <shun@ShunnoMacBook-Air.local>
2025-11-17 06:34:18 -0800

    WIP

---
+++ app/src/main/java/controller/HelloServlet.java
@@ -0,0 +1,24 @@
+package main.java.controller;
+
+import java.io.IOException;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+  private final String JSP = "/WEB-INF/jsp/Hello.jsp";
+
+  @Override
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws IOException, ServletException {
+    request.setCharacterEncoding("UTF-8");
+    response.setCharacterEncoding("UTF-8");
+    
+    var dispatcher = request.getRequestDispatcher(JSP);
+    dispatcher.forward(request, response);
+  }
+}
